3195800ad96bd8f197b509dd802ccfa186397da7
Ryan <ryangabriel_cap@hotmail.comn>
2024-03-28 21:23:23 +0000

    feat: Criado componte header utilizando module css

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { Logo } from "../assets/ignite-logo"
+import styles from "./Header.module.css"
+
+export function Header() {
+  return(
+    <header className={styles.header}>
+      <Logo/>
+      <strong>Ignite Feed</strong>
+    </header>
+  )
+}
