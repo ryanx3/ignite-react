963c504741151a16a91c1e6341a8d4bb7f681691
Ryan <ryangabriel_cap@hotmail.comn>
2024-03-28 21:24:30 +0000

    feat: Aplicado componentes no App

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
+import styles from './App.module.css'
+
+export function App() {
+  return (
+    <div>
+        <Header/>
+
+      <div className={styles.wrapper}> 
+        <Sidebar/>
+        <main>Main</main>
+      </div>
+    </div>
+  )
+}
+
+
