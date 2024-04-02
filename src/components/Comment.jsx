c20140db95d067a1b06100e0c5ab9bef5d6f971b
Ryan <ryangabriel_cap@hotmail.comn>
2024-04-02 22:56:58 +0100

    feat: Adicionando likes aos comentários

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import { ThumbsUp, Trash } from "phosphor-react";
-import styles from "./Comment.module.css";
 import { Avatar } from "./Avatar";
+import styles from "./Comment.module.css";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
-   onDeleteComment(content);
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(prevState => prevState + 1);
   }
 
   return (
@@ -30,9 +37,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
